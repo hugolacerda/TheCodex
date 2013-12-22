3e097978de1e3664b9936201e5c9db36f49ff126
Hugo Lacerda <hugolacerda20@gmail.com>
2013-12-22 15:05:48 -0500

    Final Submit

--- js/controllers/signincontroller.js
+++ js/controllers/signincontroller.js
@@ -4,10 +4,10 @@
   myapp.controller('SigninCtrl', ['$rootScope', '$scope','$firebaseAuth', '$location', function($rootScope, $scope,$firebaseAuth, $location){
   
 
-
   var ref = new Firebase("http://codexdb.firebaseio.com");
   $scope.auth = $firebaseAuth(ref, {path: "/main"});
-  console.log('$firebaseAuth',$firebaseAuth(ref));
+
+  
 
 
   $scope.signup = function(){
@@ -15,13 +15,19 @@
   }
   
   $scope.login = function(callback){
+
+  console.log('$firebaseAuth',$firebaseAuth(ref));
     console.log('$rootScope.auth',$rootScope.auth);
     console.log('$scope.loginFormInfo',$scope.loginFormInfo);
-    $scope.email = null;
-    $scope.pass = $scope.auth.$login('password', $scope.loginFormInfo);
-    $scope.err = null;
+    $rootScope.email = null;
+    $rootScope.pass = $scope.auth.$login('password', $scope.loginFormInfo);
+    $rootScope.err = null;
+    
 
   
+
+  
+
   }
 
 }])
